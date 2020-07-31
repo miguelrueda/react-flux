f8534dde865e7652f4b0d76ce449d9a74d51896d
miguelrueda <miguel.rueda.e@thalesdigital.io>
2020-07-31 15:29:43 -0500

    Module 7 completed

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,40 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
+import CourseList from "./CourseList";
 
-class CoursesPage extends React.Component {
+function CoursesPage() {
 
-    state = {
-        courses: []
-    };
+    const [courses, setCourses] = useState([]);
 
-    componentDidMount() {
+    useEffect(() => {
         getCourses()
-            .then(courses => this.setState({ courses: courses }));
-    }
+            .then(_courses => setCourses(_courses));
+    }, [setCourses]);
 
-    render() {
-        return <>
-            <h2>Courses</h2>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Author ID</th>
-                        <th>Category</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.state.courses.map(course => {
-                        return <tr key={course.id}>
-                            <td>{course.title}</td>
-                            <td>{course.authorId}</td>
-                            <td>{course.category}</td>
-                        </tr>
-                    })}
-                </tbody>
-            </table>
-        </>
-    }
+    return <>
+        <h2>Courses</h2>
+        <CourseList courses={courses} />
+    </>
 }
 
 export default CoursesPage;
